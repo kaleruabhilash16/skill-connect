93a519a487b72e259d9e66867606426b86c89aa8
Kaleru Abhilash <kaleruabhilash@gmail.com>
2025-02-06 12:12:21 +0530

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="hero-section">
+        <h1>Welcome to Skill Connect</h1>
+        <p>Collaborate with developers and work on projects in real-time.</p>
+        <button className="cta-button">Get Started</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
